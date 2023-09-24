830246b059dc75aed643710f2eeee4194a385783
s3979126 <s3979126@rmit.edu.vn>
2023-09-24 19:33:57 +0700

    save7;33

--- src/PortManagement/PortManagementSystem.java
+++ src/PortManagement/PortManagementSystem.java
@@ -4,13 +4,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PortManagementSystem {
-    public static void main(String[] args) {
+
         // Sample data initialization
         List<Port> ports = new ArrayList<>();
         List<Vehicle> vehicles = new ArrayList<>();
         List<Container> containers = new ArrayList<>();
-        List<Trip> trips = new ArrayList<>();
+        List<Trip> trips = new ArrayList<>();}
 
         // Implement the main application logic here
-    }
-}
+//        public void deletePortById(String id) {
+//            Port portToRemove = null;
+//            for (Port port : ports) {
+//                if (port.getId().equals(id)) {
+//                    portToRemove = port;
+//                    break; // Exit the loop once the matching Port is found
+//                }
+//            }
+//
+//            if (portToRemove != null) {
+//                ports.remove(portToRemove);
+//                System.out.println("Port with ID " + id + " has been removed.");
+//                writePorts.writePorts(); // Update the file after removing the Port
+//            } else {
+//                System.out.println("Port with ID " + id + " not found.");
+//            }
+//        }
+//
+//    // Load existing Ports from the file
+//    private void loadPortsFromFile() {
+//        try (Scanner scanner = new Scanner(new File(FILE_NAME))) {
+//            while (scanner.hasNextLine()) {
+//                String line = scanner.nextLine();
+//                String[] parts = line.split(",");
+//                if (parts.length >= 2) {
+//                    String id = parts[0];
+//                    String name = parts[1];
+//                    // Create a Port object and add it to the list
+//                    ports.add(new Port(id, name));
+//                }
+//            }
+//        } catch (FileNotFoundException e) {
+//            // Handle file not found exception
+//        }
+//    }
+//}
+
--- src/PortManagement/Ship.java
+++
@@ -1,6 +0,0 @@
-package PortManagement;
-
-// Class representing a Ship (extends Vehicle)
-class Ship {
-    // Implement ship-specific methods for handling containers
-}

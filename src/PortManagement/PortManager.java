e97d2cc912b0f4e484a2b5ebb121a664defab815
itspquang <119653302+itspquang@users.noreply.github.com>
2023-09-25 18:46:00 +0700

    Update PortManager.java

--- src/PortManagement/PortManager.java
+++ src/PortManagement/PortManager.java
@@ -1,19 +1,31 @@
 package PortManagement;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.util.ArrayList;
-import java.util.List;
-
 // Class representing a Port Manager
 class PortManager implements User {
     // Implement the User interface methods
-    @Override
-    public void login(String username, String password) {
-        // Implement login logic for the port manager
+    
+    private String username;
+    private String password;
+
+    // Constructor to set username and password during object creation
+    public PortManager(String username, String password) {
+        this.username = username;
+        this.password = password;
     }
+
+    // Implement the User interface method for login
+    @Override
+    public void login(String enteredUsername, String enteredPassword) {
+        // Check if the entered username and password match the Port Manager's credentials
+        if (username.equals(enteredUsername) && password.equals(enteredPassword)) {
+            System.out.println("Login successful!");
+        } else {
+            System.out.println("Login failed. Please check your credentials.");
+        }
+    }
+  
+        
+    
 
     @Override
     public void viewInformation() {
